b74e3c0f6327196bb2c36ef6f13a95be504e20c5
JEONG JAE HYEON <wolgus104@naver.com>
2020-12-17 02:32:15 +0900

    in out 개찰구 예제를 만들어서 실행.

--- src/ProducerConsumerTest.java
+++ src/ProducerConsumerTest.java
@@ -6,15 +6,15 @@ public class ProducerConsumerTest {
 
         // 사이즈를 30으로
         BlockingQueue<Message> queue = new ArrayBlockingQueue<Message>(30);
-        Producer producer = new Producer(queue);
-        Consumer consumer = new Consumer(queue);
+        inGuichet inGuichet = new inGuichet(queue);
+        outGuichet outGuichet = new outGuichet(queue);
 
-        // 생산자 큐에 메세지 생산 시작
-        new Thread(producer).start();
+        // in 개찰구 큐에 메세지 생산 시작
+        new Thread(inGuichet).start();
 
-        // 소비자 큐에 메세지 마무리 시작
-        new Thread(consumer).start();
+        // out 개찰구 큐에 메세지 마무리 시작
+        new Thread(outGuichet).start();
 
-        System.out.println("Producer Consumer Test 테스트 시작");
+        System.out.println("개찰구 테스트 시작");
     }
 }
