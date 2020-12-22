cc4e7b55de31367afbaa6911d7edcc37d6a71524
JEONG JAE HYEON <wolgus104@naver.com>
2020-12-23 03:22:04 +0900

    버블정렬 구현.

--- src/guichetController.java
+++ src/guichetController.java
@@ -7,25 +7,45 @@ public class guichetController {
     public static void main(String[] args) {
         System.out.println("guichet controller");
 
-        int i, j, min, index = 0, temp;
-        int[] num = {1, 6, 2, 4, 5, 3, 7, 8, 10, 9};
+        //선택정렬
+//        int i, j, min, index = 0, temp;
+//        int[] num = {1, 6, 2, 4, 5, 3, 7, 8, 10, 9};
+//
+//        for (i = 0; i < num.length; i++) {
+//            min = 999;
+//            for (j = i; j < num.length; j++) {
+//                if (min > num[j]) {
+//                    min = num[j];
+//                    index = j;
+//                }
+//            }
+//            // 스와핑
+//            temp = num[i];
+//            num[i] = num[index];
+//            num[index] = temp;
+//        }
+//        for (int a = 0; a < num.length; a++) {
+//            System.out.printf("%d",num[a]);
+//        }
 
-        for (i = 0; i < num.length; i++) {
-            min = 999;
-            for (j = i; j < num.length; j++) {
-                if (min > num[j]) {
-                    min = num[j];
-                    index = j;
+        //버블정렬
+        int j, i, temp;
+        int[] num = {1, 6, 2, 4, 5, 3, 7, 8, 10, 9};
+        for (j = 0; j < num.length; j++) { // n번 해줘야 전체 순환.
+            for (i = 0; i < num.length - 1; i++) { //num.length-1 n-1 번 비교
+                if (num[i] > num[i + 1]) { // 앞에 있는 값이 뒤보다 크면
+                    temp = num[i]; // 스와핑
+                    num[i] = num[i + 1];
+                    num[i + 1] = temp;
+
                 }
             }
-            // 스와핑
-            temp = num[i];
-            num[i] = num[index];
-            num[index] = temp;
         }
+
         for (int a = 0; a < num.length; a++) {
-            System.out.printf("%d",num[a]);
+            System.out.printf("%d", num[a]);
         }
     }
+
 
 }
